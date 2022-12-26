9a3cbff681b6e84c81f5a0b0af3a35995bf8b333
protondk <edgarjosue2911@gmail.com>
2022-12-26 14:33:34 -0600

    Added mobile menu. It opens and closes. Dark background when menu is open missing. Also pending to toggle mobile menu features and company

--- script/main.js
+++ script/main.js
@@ -4,6 +4,9 @@ const featuresBtn = document.getElementById("nav-features");
 const featuresMenu = document.querySelector(".features-container");
 const companyBtn = document.getElementById("nav-company");
 const companyMenu = document.querySelector(".company-container");
+const burgerMenu = document.querySelector(".brg-menu");
+const mobileMenu = document.getElementById("mobile-menu");
+const closeMenu = document.getElementById("mobile-close-icon");
 
 /* Storing each arrow icon in variables. */
 let arrowIconFeatures = document.getElementById("arrow-icon-features");
@@ -16,34 +19,36 @@ let isArrowDownDisplayedComp = true;
 /* Adding an event listener to the featuresBtn and companyBtn. */
 featuresBtn.addEventListener("click", toggleDesktopFeatures);
 companyBtn.addEventListener("click", toggleDesktopCompany);
+burgerMenu.addEventListener("click", toggleMobileMenu);
+closeMenu.addEventListener("click", closeMobileMenu);
 
 /**
  * If the arrow is down, change it to up, and vice versa
  */
 function toggleArrowIconFeatures() {
-    if (isArrowDownDisplayedFt) {
-        arrowIconFeatures.src = "../assets/icons/icon-arrow-up.svg";
-        isArrowDownDisplayedFt = false;
-    } else {
-        arrowIconFeatures.src = "../assets/icons/icon-arrow-down.svg";
-        isArrowDownDisplayedFt = true;
-    }
+  if (isArrowDownDisplayedFt) {
+    arrowIconFeatures.src = "../assets/icons/icon-arrow-up.svg";
+    isArrowDownDisplayedFt = false;
+  } else {
+    arrowIconFeatures.src = "../assets/icons/icon-arrow-down.svg";
+    isArrowDownDisplayedFt = true;
+  }
 }
 
 function toggleArrowIconCompany() {
-    if (isArrowDownDisplayedComp) {
-        arrowIconCompany.src = "../assets/icons/icon-arrow-up.svg";
-        isArrowDownDisplayedComp = false;
-    } else {
-        arrowIconCompany.src = "../assets/icons/icon-arrow-down.svg";
-        isArrowDownDisplayedComp = true;
-    }
+  if (isArrowDownDisplayedComp) {
+    arrowIconCompany.src = "../assets/icons/icon-arrow-up.svg";
+    isArrowDownDisplayedComp = false;
+  } else {
+    arrowIconCompany.src = "../assets/icons/icon-arrow-down.svg";
+    isArrowDownDisplayedComp = true;
+  }
 }
 
 /**
  * If the featuresMenu exists, toggle the classList of the featuresMenu to grid-inactive, and toggle
  * the arrow icon.
-*/
+ */
 function toggleDesktopFeatures() {
   featuresMenu?.classList.toggle("grid-inactive");
   toggleArrowIconFeatures();
@@ -52,4 +57,9 @@ function toggleDesktopCompany() {
   companyMenu?.classList.toggle("grid-inactive");
   toggleArrowIconCompany();
 }
-
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
+}
+function closeMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
+}
